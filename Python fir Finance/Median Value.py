7d36976b7fc288f672be0150898d38a2f0d6f1f9
KGrants <118312276+KGrants@users.noreply.github.com>
2023-01-07 02:51:17 +0200

    Create Median Value.py

---
+++ Python fir Finance/Median Value.py
@@ -0,0 +1,9 @@
+prices = [75000, 95000, 98000, 109000, 135500, 185000, 199000, 249000, 255000, 280000, 299000, 330000, 380000]
+
+def median(arr):
+   return prices[len(prices)//2]
+   
+
+res = median(prices)
+print(res)
+
