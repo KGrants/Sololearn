645dc6994df0646b51506ee3169ae8c93baa033e
KGrants <118312276+KGrants@users.noreply.github.com>
2023-01-15 01:02:17 +0200

    Create Drawing.cs

---
+++ C# Intermediate/Drawing.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+
+        }
+    }
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+    
+    class Brush:Draw
+    {
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using brush");
+        }
+    }
+
+    class Spray:Draw
+    {
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using spray");
+        }
+
+    }
+}
