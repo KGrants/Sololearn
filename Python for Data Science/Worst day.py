3a6dd18903841e022fcc1fb10c95797bac9d100b
KGrants <118312276+KGrants@users.noreply.github.com>
2023-01-26 08:19:33 +0200

    Create Worst day.py

---
+++ Python for Data Science/Worst day.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("/usercode/files/ca-covid.csv")
+
+df.drop('state', axis=1, inplace=True)
+df['date'] = pd.to_datetime(df['date'], format="%d.%m.%y")
+df['month'] = df['date'].dt.month_name()
+df.set_index('date', inplace=True)
+
+a = input()
+maxi = df[df["month"] == a]['cases'].max()
+print(df[df['cases'] == maxi])
