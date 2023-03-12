6f1809007ef255ff2fe9e34f8128edcb9598fbd2
KGrants <118312276+KGrants@users.noreply.github.com>
2023-03-13 00:15:51 +0200

    Create Missing Numbers.py

---
+++ Data Science (Python)/Missing Numbers.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+lst = [float(x) if x != 'nan' else np.NaN for x in input().split()]
+lst_df = pd.Series(lst)
+a = lst_df.fillna(lst_df.mean().round(1))
+print(a)
