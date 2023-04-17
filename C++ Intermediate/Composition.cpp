b6f1780200df883ec0d22f1217c64f2576dca215
KGrants <118312276+KGrants@users.noreply.github.com>
2023-04-17 19:28:12 +0300

    Create Composition.cpp

---
+++ C++ Intermediate/Composition.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Engine {
+    public:
+        Engine(int p): power(p) {};
+        void start() {
+            cout <<"Engine ON ("<<power<<" horsepower)";
+        }
+    private:
+        int power;
+};
+class Car {
+    public:
+        Car(Engine x, string c, int y): e(x), color(c), year(y) {};
+        void start() {
+            cout <<"Starting"<<endl;
+            e.start();
+            
+        }
+    private:
+        Engine e;
+        string color;
+        int year;
+};
+
+int main() {
+    int power;
+    string color;
+    int year;
+    cin >> power >> color >> year;
+    
+    Engine e(power);
+    Car c(e,"red",2023);
+    c.start();
+
+}
